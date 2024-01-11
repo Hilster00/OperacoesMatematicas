abe5a4a8cfe84c3d068e45881106832d47c04763
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2024-01-10 22:20:07 -0300

    Update and rename MDC.py to mdc&mmc.py

--- mdc&mmc.py
+++ mdc&mmc.py
@@ -29,4 +29,14 @@ def mdc(n,m):
             n=n if n%i != 0 else n/i
             m=m if m%i != 0 else m/i
     return  reduce( lambda prod, i: i*prod, lista_mdc, 1)
+
+def mmc(n,m):
+    lista_mmc=[]
+    for i in retornar_primos(n if n>m else m):
+        while (n%i==0) or (m%i==0):
+            lista_mmc.append(i)
+            n=n if n%i != 0 else n/i
+            m=m if m%i != 0 else m/i
+     return  reduce( lambda prod, i: i*prod, lista_mmc, 1)
+
     
